75a8c44d1988449740bb3e9ada8e8d3c56d2af06
Francisco González Poveda <francisco.gonzalez@sax.es>
2025-03-13 14:04:25 +0100

    cambios en el middleware de autenticacion

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,16 +3,18 @@ const jwt = require('jsonwebtoken');
 const SECRET_KEY = process.env.JWT_SECRET;
 
 const authMiddleware = (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) return res.status(401).json({ message: 'Acceso no autorizado' });
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+  if (!token) return res.status(401).json({ message: "Acceso no autorizado" });
 
   try {
     const decoded = jwt.verify(token, SECRET_KEY);
     req.user = decoded;
     next();
   } catch (error) {
-    res.status(401).json({ message: 'Token inválido o expirado' });
+    res.status(401).json({ message: "Token inválido o expirado" });
   }
 };
+
 
 module.exports = authMiddleware;
